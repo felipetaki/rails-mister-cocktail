c3b2ac99ac55a3f7efcd188eefcb37e23f5e1fb8
Felipe Taki <balcaam@gmail.com>
2020-12-01 17:15:58 -0300

    server

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'cocktails/new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails, except: [ :update, :destroy, :edit ] do
     resources :doses, only: [ :new, :create ]
   end
